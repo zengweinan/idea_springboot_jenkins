bf74246658e0e0d8277cc21c442ddb76a7b90cb4
zengweinan <40931957+zengweinan@users.noreply.github.com>
2019-06-05 16:09:18 +0800

    Update ZwnpacteraApplication.java

--- zwnpactera/src/main/java/com/pactera/ZwnpacteraApplication.java
+++ zwnpactera/src/main/java/com/pactera/ZwnpacteraApplication.java
@@ -12,10 +12,10 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAutoConfiguration//开启自动初始化
-@EnableRabbit//开启mq
-@EnableScheduling//开始定时任务注解
+#@EnableRabbit//开启mq
+#@EnableScheduling//开始定时任务注解
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@MapperScan(basePackages = {"com.pactera.mapper"})//扫码mapper
+#@MapperScan(basePackages = {"com.pactera.mapper"})//扫码mapper
 public class ZwnpacteraApplication extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
 
     public static void main(String[] args) {
